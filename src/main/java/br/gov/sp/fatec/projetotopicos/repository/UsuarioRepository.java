ccd2a584700a349a9ef11dc6268c479f51a8cf43
Pedro Reginaldo Tome Silva <67759198+PedroSilva201@users.noreply.github.com>
2022-10-31 07:13:57 -0300

    Delete src directory

--- src/main/java/br/gov/sp/fatec/projetotopicos/repository/UsuarioRepository.java
+++
@@ -1,9 +0,0 @@
-package br.gov.sp.fatec.projetotopicos.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import br.gov.sp.fatec.projetotopicos.entity.Usuario;
-
-public interface UsuarioRepository extends JpaRepository<Usuario, Long> {
-    
-}
